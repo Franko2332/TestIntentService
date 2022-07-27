2b21e4b0c73fe6c12beace26ae5563384496d578
Franko777 <khubul.george@inbox.ru>
2022-07-27 12:19:56 +0300

    Initial commit

---
+++ app/src/main/java/ru/gb/testintentservice/MainActivity.kt
@@ -0,0 +1,17 @@
+package ru.gb.testintentservice
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val intent = Intent().apply {
+            setAction("ru.gb.thegithubclient.service.CustomIntentService")
+            setPackage("ru.gb.thegithubclient")
+        }
+            startService(intent)
+        }
+    }
